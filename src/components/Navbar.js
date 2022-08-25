51c32d61f7c49f8ef26b37255365fa1b37756db7
Gabriel Silva Santos <33208964+gabs111@users.noreply.github.com>
2022-08-25 17:21:53 -0300

    search-coin

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaCoins } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <Link to="/">
+      <div>
+        <div className="navbar">
+          <FaCoins className="icon" />
+          <h1>
+            Crypto <span className="purple">Search</span>
+          </h1>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Navbar;
